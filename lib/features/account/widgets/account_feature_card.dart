74c942785f2780c61f061b3fe58a5299e573522c
nikilasilva <nikilasilva@gmail.com>
2025-07-03 01:29:27 +0530

    feat: add onTap feature to 'AccountFeatureCard'

--- lib/features/account/widgets/account_feature_card.dart
+++ lib/features/account/widgets/account_feature_card.dart
@@ -6,47 +6,52 @@ class AccountFeatureCard extends StatelessWidget {
   final String title;
   final String subtitle;
   final Widget icon; // can be an Icon or an Image;
+  final VoidCallback? onTap;
 
   const AccountFeatureCard({
     super.key,
     required this.title,
     required this.subtitle,
     required this.icon,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(AppDimensions.widgetSpacing),
-      decoration: BoxDecoration(
-        color: AppColors.subtleGrey,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: AppTextStyles.bodyLarge.copyWith(
-                    fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(AppDimensions.widgetSpacing),
+        decoration: BoxDecoration(
+          color: AppColors.subtleGrey,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: AppTextStyles.bodyLarge.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  subtitle,
-                  style: AppTextStyles.bodySmall.copyWith(
-                    color: AppColors.textSecondary,
+                  const SizedBox(height: 4),
+                  Text(
+                    subtitle,
+                    style: AppTextStyles.bodySmall.copyWith(
+                      color: AppColors.textSecondary,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          const SizedBox(width: AppDimensions.widgetSpacing),
-          icon,
-        ],
+            const SizedBox(width: AppDimensions.widgetSpacing),
+            icon,
+          ],
+        ),
       ),
     );
   }
